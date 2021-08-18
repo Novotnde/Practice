823e5b2beadbcebdf33beb8d45ec075acf7e5a0c
Novotnde <novotnde@gmail.com>
2021-08-18 21:18:20 +0200

    test

--- Core/Core/Program.cs
+++ Core/Core/Program.cs
@@ -7,7 +7,7 @@ namespace Core
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             const int number = 10;
             var numbers = new List<int>() {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
